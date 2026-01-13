afccec0fb3df93d98f13615e0c94f22f0786a1a7
Yehor Rumiantsev <y.rumiantsev@favbet.tech>
2026-01-13 23:27:51 +0100

    switch to hook

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"microservice/service"
+	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
@@ -18,51 +22,121 @@ import (
 var db *pgxpool.Pool
 
 func main() {
-	runMigrations()
+	dsn := os.Getenv("DATABASE_URL")
+	service.RunMigrations(dsn)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	webhookURL := os.Getenv("WEBHOOK_URL")
+	token := os.Getenv("BOT_TOKEN")
+
+	if token == "" || webhookURL == "" {
+		log.Fatal("TG_TOKEN or WEBHOOK_URL not set")
+	}
 
 	ctx := context.Background()
 
 	var err error
-	db, err = pgxpool.New(ctx, os.Getenv("DATABASE_URL"))
+	db, err = pgxpool.New(ctx, dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
+	bot.Debug = false
+	log.Printf("Authorized as %s", bot.Self.UserName)
 
-	for update := range updates {
-
-		if update.Message != nil && update.Message.IsCommand() {
-			switch update.Message.Command() {
-
-			case "add":
-				handleAdd(bot, update)
-
-			case "players":
-				sendPlayers(bot, update.Message.Chat.ID)
-			}
-		}
-
-		if update.CallbackQuery != nil {
-			data := update.CallbackQuery.Data
-			parts := strings.Split(data, ":")
-
-			switch parts[0] {
-
-			case "minus":
-				handleClickDecrease(bot, update, parts[1])
-			case "plus":
-				handleClickIncrease(bot, update, parts[1])
-			}
-		}
+	// включаем webhook
+	wh, err := tgbotapi.NewWebhook(webhookURL)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	if _, err := bot.Request(wh); err != nil {
+		log.Fatal(err)
+	}
+
+	info, _ := bot.GetWebhookInfo()
+	log.Printf("Webhook set to %s", info.URL)
+
+	updates := bot.ListenForWebhook("/webhook")
+
+	server := &http.Server{Addr: ":" + port}
+
+	// сигналы
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("HTTP server started on :%s", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+	// обработка апдейтов
+	go func() {
+		for update := range updates {
+			handleUpdate(bot, update)
+		}
+	}()
+
+	<-stop
+	log.Println("Shutdown signal received")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// выключаем webhook
+	if _, err := bot.Request(tgbotapi.DeleteWebhookConfig{}); err != nil {
+		log.Printf("DeleteWebhook error: %v", err)
+	} else {
+		log.Println("Webhook disabled")
+	}
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("HTTP shutdown error: %v", err)
+	}
+
+	log.Println("Bot stopped gracefully")
+
+	// u := tgbotapi.NewUpdate(0)
+	// u.Timeout = 60
+	// updates := bot.GetUpdatesChan(u)
+
+	// for update := range updates {
+
+	// 	if update.Message != nil && update.Message.IsCommand() {
+	// 		switch update.Message.Command() {
+
+	// 		case "add":
+	// 			handleAdd(bot, update)
+
+	// 		case "players":
+	// 			sendPlayers(bot, update.Message.Chat.ID)
+	// 		}
+	// 	}
+
+	// 	if update.CallbackQuery != nil {
+	// 		data := update.CallbackQuery.Data
+	// 		parts := strings.Split(data, ":")
+
+	// 		switch parts[0] {
+
+	// 		case "minus":
+	// 			handleClickDecrease(bot, update, parts[1])
+	// 		case "plus":
+	// 			handleClickIncrease(bot, update, parts[1])
+	// 		}
+	// 	}
+	// }
 }
 
 func handleAdd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
@@ -184,15 +258,42 @@ func handleClickDecrease(bot *tgbotapi.BotAPI, update tgbotapi.Update, name stri
 	bot.Request(cb)
 }
 
-func runMigrations() {
-	m, err := migrate.New("file://migrations", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
+func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+
+	if update.Message != nil && update.Message.IsCommand() {
+		switch update.Message.Command() {
+
+		case "add":
+			handleAdd(bot, update)
+
+		case "players":
+			sendPlayers(bot, update.Message.Chat.ID)
+		}
 	}
 
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
+	if update.CallbackQuery != nil {
+		data := update.CallbackQuery.Data
+		parts := strings.Split(data, ":")
+
+		switch parts[0] {
+
+		case "minus":
+			handleClickDecrease(bot, update, parts[1])
+		case "plus":
+			handleClickIncrease(bot, update, parts[1])
+		}
 	}
 
-	log.Println("migrations applied")
+	log.Printf(
+		"Message from %d: %s",
+		update.Message.Chat.ID,
+		update.Message.Text,
+	)
+
+	msg := tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		"Привет! Я получил твоё сообщение 👍",
+	)
+
+	bot.Send(msg)
 }
